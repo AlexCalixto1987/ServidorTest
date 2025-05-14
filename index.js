3c5d432bfbbb4697c32f778a3da8d692e4882887
Alejandro Calixto <alejandrocalixto@MacBook-Pro-de-Alejandro.local>
2025-05-13 18:19:41 -0600

    Se ajusta a la nueva estructura de register-token

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 // Base de datos simulada (array en memoria)
 let registeredTokens = [];
+let device = []
+let model = []
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Ruta para registrar el token
 app.post("/register-token", (req, res) => {
-    const { token } = req.body;
+    const { token, device, ref01, ref02, ref03 } = req.body;
 
     if (!token) {
         console.log("TOKEN REQUERIDO")
@@ -23,6 +25,16 @@ app.post("/register-token", (req, res) => {
     if (!registeredTokens.includes(token)) {
         registeredTokens.push(token);
         console.log("Token registrado:", token);
+    }
+
+    if (!device.includes(device)) {
+        device.push(device);
+        console.log("device: ", device);
+    }
+
+    if (!model.includes(ref01)) {
+        model.push(ref01);
+        console.log("model:", ref01);
     }
 
     return res.status(200).json({ message: "Token registrado con éxito" });
